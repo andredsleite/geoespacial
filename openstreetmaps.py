48a0cc362b7933abc5456bb07bc2f2b71991db6d
andredsleite <42873060+andredsleite@users.noreply.github.com>
2019-01-05 19:22:27 -0200

    Update openstreetmaps.py

--- openstreetmaps.py
+++ openstreetmaps.py
@@ -7,7 +7,7 @@ import matplotlib
 # !jupyter nbextension enable --py --sys-prefix ipyleaflet; para ativar a extensao
 import ipyleaflet
 from ipyleaflet import *
-map1= Map(center=[ -15.779,-47.929],zoom=10)
+map1= Map(center=[ -15.779,-47.929],zoom=10) #coordenadas de brasília
 map1; #openstreetmaps basico
 map1.layers =[basemap_to_tiles(basemaps.CartoDB.DarkMatter)] 
 map1; #base com fundo preto. para acessar outras bases digite: basemaps.'tab'(a tecla tab mesmo)
